128d5ed785cde18d4cc5b8446d130c8c5b40e9e2
AndresAfar <afar2503@gmail.com>
2024-01-23 14:48:57 -0500

    add component status

---
+++ src/components/StatusWork.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const StatusWork = () => {
+  return (
+    <a href="" className="flex justify-center items-center hover:scale-105 transition">
+        <div className="cursor-pointer text-sm px-3 py-1 font-medium text-white rounded-full bg-slate-950/90 ring-1 ring-bunker-500/20 ring-[#79c97d]">
+            Disponible para trabajar
+        </div>
+    </a>
+  );
+};
