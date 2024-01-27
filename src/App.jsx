8a58d4d43479c144a59b81a20031ff17e8c73b6a
AndresAfar <afar2503@gmail.com>
2024-01-27 15:42:08 -0500

    modified bg

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,20 @@ import React from 'react'
 
 import { Route, Routes } from 'react-router-dom'
 
+import { Footer } from './components/Footer.jsx'
+import { Component } from './components/Navbarcom.jsx'
+
 import { Home } from './pages/Home'
 import './App.css'
 
 function App() {
   return (
-    <div className='App'>
+    <div className='App bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]'>
+      <Component/>
       <Routes>
         <Route path="/" element={<Home/>} />
       </Routes>
+      <Footer/>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,14 @@ import App from './App.jsx'
 import './index.css'
 
 import {Navbar} from './components/Navbar.jsx' 
-import { Footer } from './components/Footer.jsx'
-import { Component } from './components/Navbarcom.jsx'
+
 
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Component/>
       <App />
-      <Footer/>
     </BrowserRouter>
   </React.StrictMode>,
 )
