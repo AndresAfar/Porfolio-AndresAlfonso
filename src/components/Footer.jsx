dd5180cbd4073b13c1666c93ede5c62336da77a1
AndresAfar <afar2503@gmail.com>
2024-02-06 21:54:28 -0500

    modified component footer and style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <footer className="bg-gray-700/30 rounded-lg shadow m-4 ">
-        <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+    <footer className="bg-gray-700/30 rounded-lg shadow m-4">
+        <div className="max-w-screen-xl mx-auto p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-red-300/90 sm:text-center dark:text-gray-400">© 2023 <a href="#" className="hover:underline">Andres_Afar</a>. Todos los derechos reservados.
         </span>
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-white/90 dark:text-gray-400 sm:mt-0">
