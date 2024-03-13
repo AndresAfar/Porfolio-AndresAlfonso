08290ca990b0dfba816a975d60e21feacbc287e1
AndresAfar <afar2503@gmail.com>
2024-03-13 12:39:41 -0500

    modified btn cv

--- src/components/Navbarcom.jsx
+++ src/components/Navbarcom.jsx
@@ -20,7 +20,7 @@ export function Component() {
         <NavbarLink href="#concat" className='transition border-0 py-2 px-2 md:p-0 rounded hover:bg-gray-100 md:hover:bg-transparent  md:hover:text-[#df9a9a] text-[#a7b4c0]' >
             Contacto
         </NavbarLink>
-        <NavLink to={'/src/assets/docs/CV_AFAR.pdf'} target='_blank' download={'CV_AndresAlfonso.pdf'} className='p-2 flex items-center gap-x-1 font-bold rounded hover:-translate-y-0.5 text-[#e26767] hover:bg-red-300/10 transition duration-150 border-2 border-red-300/80'>
+        <NavLink to={'/src/assets/docs/CV_AFAR.pdf'} download={'CV_AndresAlfonso.pdf'} className='p-2 flex items-center gap-x-1 font-bold rounded hover:-translate-y-0.5 text-[#e26767] hover:bg-red-300/10 transition duration-150 border-2 border-red-300/80'>
             CV
             <svg className="w-6 h-6 text-[#e26767] dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
                 <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2Zm-3 15H4.828a1 1 0 0 1 0-2h6.238a1 1 0 0 1 0 2Zm0-4H4.828a1 1 0 0 1 0-2h6.238a1 1 0 1 1 0 2Z"/>
