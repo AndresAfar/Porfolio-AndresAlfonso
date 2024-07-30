685e88ff3a256c90c05d17e12d122df2b582be03
AndresAfar <afar2503@gmail.com>
2024-07-29 21:12:49 -0500

    modified pdf link and dependencies

--- src/components/StatusWork.jsx
+++ src/components/StatusWork.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const StatusWork = () => {
   return (
-    <a href="https://www.linkedin.com/in/andres-alfonso-rojas-web-developer/" target="_blank" className="flex justify-center items-center hover:scale-105 transition mt-2">
+    <a href="https://www.linkedin.com/in/andres-alfonso-rojas/" target="_blank" className="flex justify-center items-center hover:scale-105 transition mt-2">
         <div className="cursor-pointer text-sm px-3 py-1 font-medium text-white rounded-full bg-slate-950/90 ring-1 ring-bunker-500/20 ring-[#79c97d]">
             Disponible
         </div>
